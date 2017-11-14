38946124cc7089c6227d81cad87959171aeece76
alice1009tw <alice1009tw@gmail.com>
2017-11-14 10:48:01 +0800

    Guess

--- app/src/main/java/com/fju/guess100/MainActivity.java
+++ app/src/main/java/com/fju/guess100/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fju.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +8,40 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    public void findViews(){
-        TextView secret = (TextView)findViewById(R.id.secret);
-        TextView info = (TextView)findViewById(R.id.info);
-        EditText number = (EditText)findViewById(R.id.number);
-        Button send = (Button)findViewById(R.id.send);
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViews();
-    }
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    View.OnClickListener listener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            TextView tvsecret = (TextView) findViewById(R.id.secret);
+            Random random = new Random();
+            int r = random.nextInt(99) + 1;
+            int secret = Integer.parseInt(tvsecret.getText().toString());
+            secret = r;
+            EditText ednumber = (EditText) findViewById(R.id.number);
+            int number = Integer.parseInt(ednumber.getText().toString());
+            while (number > secret) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("0~" + number)
+                        .show();
+                break;
+            }
+        }
+
+        public void findViews() {
+            TextView tvsecret = (TextView) findViewById(R.id.secret);
+            TextView tvinfo = (TextView) findViewById(R.id.info);
+            EditText ednumber = (EditText) findViewById(R.id.number);
+            Button btsend = (Button) findViewById(R.id.send);
+        }
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            findViews();
+        }
+
+    }
 }
